78c3305c3c7143e837de07630e4484102b7db201
AqsaIftikhar88 <121974121+AqsaIftikhar88@users.noreply.github.com>
2026-04-06 00:03:01 +0500

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,28 @@
+USE [webproject]
+GO
+/****** Object:  Table [dbo].[Admin_login]    Script Date: 4/5/2026 11:59:17 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Admin_login](
+	[admin_name] [nvarchar](50) NOT NULL,
+	[password] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_Admin_login] PRIMARY KEY CLUSTERED 
+(
+	[admin_name] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[user_table]    Script Date: 4/5/2026 11:59:17 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[user_table](
+	[user_name] [nvarchar](50) NOT NULL,
+	[email_address] [nvarchar](50) NOT NULL,
+	[password] [nvarchar](50) NOT NULL,
+	[confirm_password] [nvarchar](50) NOT NULL
+) ON [PRIMARY]
+GO
